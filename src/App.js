cd56d19b0149e5694278734579db9667aa8e0fcf
DiegoDelgadillo <ddcabrera69@gmail.com>
2021-08-26 13:55:40 -0400

    V4

--- src/App.js
+++ src/App.js
@@ -2,43 +2,82 @@ const nombre = document.querySelector("#nombre-input");
 const form = document.querySelector("#saludador-form");
 const genero = document.querySelector("#genero-input");
 const edad = document.querySelector("#edad-input");
+const idioma = document.querySelector("#idioma-input")
 
 form.addEventListener("submit", (event) => {
-    var gen = "Señor ";
 
-    if(genero.value == 'F')
-    {
-        if(edad.value > 40)
+    if(idioma.value == 'E'){
+        var gen = "Señor ";
+
+        if(genero.value == 'F')
         {
-            gen = "Señora ";
+            if(edad.value > 40)
+            {
+                gen = "Señora ";
+            }
+            else{
+                gen = "Señorita "
+            }
+        }
+        if(genero.value == 'M')
+        {
+            if(edad.value < 40)
+            {
+                gen = "Joven ";
+            }
+        }
+
+        var hoy = new Date();
+        var hora = hoy.getHours();
+
+        var tiempo = "Buenos dias, ";
+        if(hora > 12 && hora < 19)
+        {
+            tiempo = "Buenas tardes, ";
         }
         else{
-            gen = "Señorita "
+            if(hora >= 19 )
+            {
+                tiempo = "Buenas noches, ";
+            }
         }
+        alert("Hola, " + tiempo +  gen + nombre.value);
     }
-    if(genero.value == 'M')
-    {
-        if(edad.value < 40)
+    if(idioma.value == 'I'){
+        var gen = "Mister ";
+
+        if(genero.value == 'F')
         {
-            gen = "Joven ";
+            if(edad.value > 40)
+            {
+                gen = "Ms ";
+            }
+            else{
+                gen = "Miss "
+            }
         }
-    }
-
-    var hoy = new Date();
-    var hora = hoy.getHours();
-
-    var tiempo = "Buenos dias, ";
-    if(hora > 12 && hora < 19)
-    {
-        tiempo = "Buenas tardes, ";
-    }
-    else{
-        if(hora >= 19 )
+        if(genero.value == 'M')
         {
-            tiempo = "Buenas noches, ";
+            if(edad.value < 40)
+            {
+                gen = "Bro ";
+            }
         }
+
+        var hoy = new Date();
+        var hora = hoy.getHours();
+
+        var tiempo = "Good morning ";
+        if(hora > 12 && hora < 19)
+        {
+            tiempo = "Good afternoon ";
+        }
+        else{
+            if(hora >= 19 )
+            {
+                tiempo = "Good evening ";
+            }
+        }
+    alert("Hi " + tiempo +  gen + nombre.value);
     }
-
-
-  alert("Hola, " + tiempo +  gen + nombre.value);
 });
